cddba4e5e7fa0221ea50138fa9b4591ba10bec73
柯剑烽 <jianfengke@infinite.net.cn>
2020-05-27 02:38:24 +0800

    完善样式

--- src/components/HeadBar/index.js
+++ src/components/HeadBar/index.js
@@ -1,58 +1,58 @@
 import React, { Component } from "react";
-import "./index.scss";
+import styles from  "./index.module.scss";
 import { NavLink} from "react-router-dom";
 class HeadBar extends Component {
   render() {
     return (
       <nav
-        className="head-bar"
+        className={styles.head_bar}
         style={{ background: this.props.navBg || "#132332" }}
       >
-        <NavLink to="/" className="logo ml20">
+        <NavLink to="/" className={`${styles.logo} ml20`}>
           <img
             src={require("../../assets/images/Jamki-logo.png")}
             alt="."
           ></img>
         </NavLink>
-        <div className="menu">
-          <ul className="menu-list">
-          <li className="menu-item">
+        <div className={styles.menu}>
+          <ul className={styles.menu_list}>
+          <li className={styles.menu_item}>
               <NavLink to="/">
                 首页
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/article">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/article">
                 文章
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/blog">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/blog">
                 行博
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/archives">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/archives">
                 归档
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/pic-works">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/pic-works">
                 图片作品
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/source">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/source">
                 资源分享
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/message">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/message">
                 留言
               </NavLink>
             </li>
-            <li className="menu-item">
-              <NavLink activeClassName="active-item" to="/about">
+            <li className={styles.menu_item}>
+              <NavLink activeClassName={styles.active_item} to="/about">
                 关于
               </NavLink>
             </li>
