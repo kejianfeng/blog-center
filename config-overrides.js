b4499bec21027c229890cdae022e0bc9966d790b
柯剑烽 <jianfengke@infinite.net.cn>
2020-05-27 02:21:51 +0800

    优化样式配置

--- config-overrides.js
+++ config-overrides.js
@@ -1,23 +1,12 @@
-const { override , fixBabelImports, addWebpackAlias} = require('customize-cra');
-
-const removeManifest = () => config => {
-    config.plugins = config.plugins.filter(
-        p => p.constructor.name !== "ManifestPlugin"
-    );
-    return config;
-};
+const { override , fixBabelImports} = require('customize-cra');
 
 module.exports = {
     webpack: override(
-        removeManifest(),
         fixBabelImports('import', {
             libraryName: 'antd',
             libraryDirectory: 'es',
             style: 'css',
-        }),
-        addWebpackAlias({
-            "@": './src'
-        }),
+        })
     ),
     // devServer: overrideDevServer(
     //     ...
